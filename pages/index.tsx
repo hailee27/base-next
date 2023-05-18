b5609202be908f11eebc1ec4057d0696196e4258
colors <colors2711@gmail.com>
2023-05-18 21:14:04 +0700

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from '@/styles/Home.module.css'
 import Home from '@/src/components/Home'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function index() {
-  const a = 'a'
+  // const a = 'a'
   console.log('alo')
   return <Home />
 }
