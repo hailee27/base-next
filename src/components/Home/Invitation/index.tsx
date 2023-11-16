dc1a0f7c8ced4adea757f652969664bdb0438ef6
colors <colors2711@gmail.com>
2023-11-17 02:56:08 +0700

    commit

--- src/components/Home/Invitation/index.tsx
+++ src/components/Home/Invitation/index.tsx
@@ -1,30 +1,30 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
 function Invitation() {
-  const [form, setForm] = useState({
-    name: '',
-    appect: ''
-  });
+  // const [form, setForm] = useState({
+  //   name: '',
+  //   appect: ''
+  // });
   // entry.1807568969=Ng%E1%BB%8Dc+%C3%81nh&entry.338718565=0393962336&entry.105458791=C%C3%B3
   // 'entry.581823759': form.name,
   // 'entry.1600231485': form.appect
-  const handleSubmit = useCallback(async () => {
-    try {
-      await fetch(
-        String(process.env.NEXT_PUBLIC_API_GG_FORM_NHA_GAI) +
-          new URLSearchParams({
-            'entry.1807568969': form.name,
-            'entry.105458791': form.appect,
-            'entry.338718565': '0123456789'
-          }),
-        {
-          mode: 'no-cors'
-        }
-      );
-    } catch (e) {
-      console.log(e);
-    }
-  }, [form]);
+  // const handleSubmit = useCallback(async () => {
+  //   try {
+  //     await fetch(
+  //       String(process.env.NEXT_PUBLIC_API_GG_FORM_NHA_GAI) +
+  //         new URLSearchParams({
+  //           'entry.1807568969': form.name,
+  //           'entry.105458791': form.appect,
+  //           'entry.338718565': '0123456789'
+  //         }),
+  //       {
+  //         mode: 'no-cors'
+  //       }
+  //     );
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }, [form]);
   return (
     <div className="w-full bg-[#ffe3e3] min-h-screen flex flex-col items-center ">
       <div className="w-full flex flex-col px-[20px] flex-1 h-full">
